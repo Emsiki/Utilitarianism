ba254ef022f154f6ac96e73a527fc0ef9a56fad6
Emsiki <jemersonking@gmail.com>
2023-04-12 17:19:20 -0400

    Started working on classes, nothing crazy

--- Utility/Person.cs
+++ Utility/Person.cs
@@ -1,11 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+
+
+
 
 public class Person
 {
-	public Person()
+	// Static arrtibute actions will contain all available actions that every member of the base class 'Person' will be able to perform
+
+
+
+
+	protected static Dictionary<string, float> actions = new Dictionary<string, float>()
 	{
 
+		{"take", 1.0f},
+		{"want",1.0f},
+		{"have", 1.0f},
+		{"go", 1.0f},
+		{"strole", 1.0f},
+		{"work", 1.0f},
+		{"eat", 1.0f},
+		{"express", 1.0f},
+		{"make", 1.0f},
+		{"feel", 1.0f},
+		{"fell", 1.0f},
+		{"listen", 1.0f},
+		{"hit", 1.0f},
+		{"defend", 1.0f},
+		{"yell", 1.0f},
+		{"hurt", 1.0f}
+
+	};
 
 
+	static protected float totalHappieness;
+
+
+
+
+
+	// Feilds
+
+	protected float happieness;
+	protected int amoutOfDirectFamily;
+	protected Personality personality;
+	
+
+
+	
+	public Person(float happieness, int amountOfDirectFamily, Personality personality,)
+	{    	
+			
+		this.amoutOfDirectFamily = amountOfDirectFamily;
+
+		this.happieness = happieness;
+
+		this.personality = personality;
+
+
+	
+	
 	}
+
+
+
+
 }
---
+++ Utility/Personality.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class Personality
+{
+    // Statics
+
+
+
+    
+
+    
+
+
+    // Feilds
+    int prejudice;
+    int agreeableness;
+    int conscientiousness;
+    // neuroticism will make it so the person struggles to regain happiness at a faster rate
+    int neuroticism;
+    // a positive intext value means they are more extroverted and a negitive more introverted
+    int introExtro;
+    int hotHeadedness;
+
+
+
+    // The randomness feild will aid in the making of a totally random personality, assigning different values for each of the regular feilds (Non-temp)
+    int randomness;
+
+
+
+    //Temp
+    // all of the temp feilds will start at 0, and they will go up depending on the persons actions and how other people interact with them
+    // the temp feilds will slowly turn into 
+    int TempPrejudice = 0;
+    int TempAgreeableness = 0;
+    int TempConscientiousness = 0;
+    int TempNeuroticism = 0;
+    int TempIntroExtro = 0;
+    int TempHotHeadedness = 0;
+
+
+
+    public Personality(int randomness)
+	{
+        this.randomness = randomness;
+                 
+        
+        
+
+
+    }
+
+
+   
+
+
+
+}
